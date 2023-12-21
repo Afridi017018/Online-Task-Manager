33ba5f076f30ce5f7e59995795847cef3f43e9e4
Afridi017018 <93644242+Afridi017018@users.noreply.github.com>
2023-12-22 02:36:46 +0600

    added profile page

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useContext } from 'react';
+import { AuthContext } from '../../providers/AuthProvider';
 
 const UserProfile = () => {
+
+    const {user} = useContext(AuthContext)
+
     return (
         <div>
-            Profile
+
+           <div className='w-1/2 mx-auto p-5 my-20'>
+
+        
+           <img className='h-44 w-44 rounded-full p-1 border border-gray-600 mx-auto my-10 shadow-xl' src= {user?.photoURL} alt="" />
+
+           <h2 className='text lg:text-2xl font-bold my-2 text-center'>Shahid Afridi </h2>
+           <h2 className='text lg:text-2xl font-bold my-2 text-center'>ssssshahidafridi017018@gmail.com</h2>
+           
+           
+           </div>
+
         </div>
     );
 };
